fc53c25f3f429d8f51d21d76b08c4650fbb713c0
Chris Wilkes <cwilkes@gmail.com>
2014-05-30 15:16:15 -0700

    show compare in util

--- util.py
+++ util.py
@@ -1,15 +1,24 @@
 import sys
 import models
+import math
 
 
 def compare_entries(entries, compare_func):
     for pos, entry1 in enumerate(entries):
-        for entry2 in entries[entry1+1:]:
+        for entry2 in entries[pos+1:]:
             yield entry1, entry2, compare_func(entry1, entry2)
 
 
-if __name__ == '__init__':
-    orders = list(models.get_orders(open(sys.argv[1])))
-    for e1, e2, distance in compare_entries(orders):
+def compare_by_lat_long(e1, e2):
+    return math.sqrt(math.pow(e1.latitude-e2.latitude, 2) + math.pow(e1.longitude-e2.longitude, 2))
+
+
+def main(args):
+    orders = list(models.get_orders(open(args[1])))
+    for e1, e2, distance in compare_entries(orders, compare_by_lat_long):
         print e1, e2, distance
+
+
+if __name__ == '__main__':
+    main(sys.argv)
 
